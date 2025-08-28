3c8d49ac4e1b4bfe8b5d621de24977f2f8dd098b
Riley Joncas <rbjoncas@gmail.com>
2025-08-28 14:24:40 -0400

    Have a skeleton for the calculator made

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Error: Invalid operator.";
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "Error: Division by zero is not allowed.";
+    }
+    return num1 / num2;
+}
